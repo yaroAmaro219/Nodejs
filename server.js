e174dce7d0d0f2ed6b9e49a5cde96d5a55dff609
yaroamaro219 <yaroamari@gmail.com>
2021-09-07 12:53:20 -0400

    commit

--- server.js
+++ server.js
@@ -1,36 +1,46 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-const { message } = require('statuses')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
+var TOKEN = proces.env.MONGODB_TOKEN
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-  {
-    name: 'Tim', message: 'Hi'
-  },
-  {
-    name: 'Art', message: 'Hi'
-  }
-]
+var dbUrl =  `mongodb+srv://user:${TOKEN}@learning-node.iwwit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+
+var Message = mongoose.model('Message', {
+  name: String,
+  message: String
+})
 
 app.get('/messages', (req, res) => {
-  res.send(messages)
+  Message.find({}, (err, messages) => {
+    res.send(messages)
+  })
 })
 
 app.post('/messages', (req, res) => {
-  messages.push(req.body)
-  io.emit('message', req.body)
-  res.sendStatus(200)
+  var message = new Message(req.body)
+
+  message.save((err) => {
+    if(err)
+      sendStatus(500)
+
+    io.emit('message', req.body)
+    res.sendStatus(200)
+  })
 })
 
 io.on('connection', (socket) => {
-  console.log('user connected')
+  console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, (err) => {
+  console.log('mongo db connection', err)
 })
 
 var server = http.listen(3000, () => {
