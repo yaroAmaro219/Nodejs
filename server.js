e1d1e245e39bd4e0fbe45452d658fa1bd5b7424e
yaroamaro219 <yaroamari@gmail.com>
2021-09-07 13:09:27 -0400

    findOne

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var mongoose = require('mongoose')
-var TOKEN = proces.env.MONGODB_TOKEN
+var TOKEN = ''
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.Promise = Promise
 
 var dbUrl =  `mongodb+srv://user:${TOKEN}@learning-node.iwwit.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
@@ -26,12 +28,21 @@ app.get('/messages', (req, res) => {
 app.post('/messages', (req, res) => {
   var message = new Message(req.body)
 
-  message.save((err) => {
-    if(err)
-      sendStatus(500)
+  message.save().then(() => {
+      Message.findOne({message: 'badword'}, (err, censored) => {
+        if(censored) {
+          console.log('censored words found', censored)
+          Message.remove({_id: censored.id}, (err) => {
+            console.log('censored word removed')
+          })
+        }
+      })
 
     io.emit('message', req.body)
     res.sendStatus(200)
+  }).catch((err) => {
+    res.sendStatus(500)
+    return console.error(err)
   })
 })
 
